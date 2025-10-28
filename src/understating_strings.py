f7e8f1a220e122f431bdad0d530b34703b8f5840
Zuritark09ship <2530071@upv.edu.mx>
2025-10-28 10:40:08 -0600

    feat: understating string

---
+++ src/understating_strings.py
@@ -0,0 +1,18 @@
+"""
+string variables
+
+un string e de manera sencilla una serie de caracteres .
+en python todo lo que se encuentre dentro de comillas simples o dobles 
+sera considerado un string.
+
+ejemplos 
+"esto es un string"
+`esto tambien es un string`
+
+`le dije a un amigo "python es mi lenguaje favorito"`
+" el lenguaje `python` lleva el nombre por Mony python , no por la
+serpiente"
+
+
+
+"""
