dcc8bbab97ac9b248f2e48cd1cc4d8ac1c269fea
Zuritark09ship <2530071@upv.edu.mx>
2025-11-20 20:12:10 -0600

    understating_dictionaris

---
+++ src/understating_dictionaries.py
@@ -0,0 +1,25 @@
+#empy ditionary
+
+homer_0={"color": "yelow",
+    "bag":"homer-donut", 
+    "hair": "black",
+    "Dress":"casual",}
+print(homer_0)
+print(type(homer_0))
+
+marge_0={"color": "yelow",
+    "bag":"homer-donut",
+    "hair": "blue", 
+    "Dress":"casual",}
+scar_0={"color": "yelow",
+    "headshot":1.5,}
+
+print(marge_0["bag"])
+print(marge_0["color"])
+
+#agregar elemenosa un diccionario 
+
+homer_0["x-posicion"]=15
+homer_0["y-posicion"]=20
+homer_0["z-posicion"]=10
+print(homer_0)
