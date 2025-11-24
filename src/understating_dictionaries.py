92e09235a7cbfc86710b89d01e7d539a25a341c7
Zuritark09ship <2530071@upv.edu.mx>
2025-11-24 10:40:40 -0600

    dictionaries

--- src/understating_dictionaries.py
+++ src/understating_dictionaries.py
@@ -1,5 +1,8 @@
 #empy ditionary
 
+from tokenize import Name
+
+
 homer_0={"color": "yelow",
     "bag":"homer-donut", 
     "hair": "black",
@@ -23,3 +26,21 @@ homer_0["x-posicion"]=15
 homer_0["y-posicion"]=20
 homer_0["z-posicion"]=10
 print(homer_0)  
+
+alien_2=["color": "blue"] 
+alien_2["x-position"]= 0
+alien_2["y-position"]= 25
+print(alien_2)
+
+Name["name"] = "paul"
+
+for key, value in alien_2.items():
+    print(f"the key{key} has value {value}")
+    
+##lopping thoung keys}
+for key in alien_2.keys():
+    print(key)
+
+##looping though items
+for item in alien_2.items():
+    print(item)
